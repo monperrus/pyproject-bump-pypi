36a6dd170e07d7f795d609342661c38f3f594968
Martin Monperrus <martin.monperrus@gnieh.org>
2024-05-18 10:07:54 +0200

    up

--- README.md
+++ README.md
@@ -1,21 +1,9 @@
-# pyproject-bump-version
-A GitHub Action to bump pyproject.toml version numbers during pull requests
+# pyproject-bump-pypi
+A GitHub Action to bump pyproject.toml version number from latest version on Pypi.
 
-This action will bump the version number of `pyproject.toml` file with respect to the main branch.
-
-For instance, say the version numbers in `branch1` and `main` are both `1.0.0` and a pull request is being made from `branch1` into `main`, then this action will bump the version number to `2.0.0` (if you specify `major` as the `bump_type` input).
-
-Note, that this action will only bump the version if the version number in main is less than or equal to the compare branch version.
-
-You can control whether you want the bump to happen before merging or after by checking the variable `github.event.pull_request.merged`.
-
+Fork of https://github.com/apowis/pyproject-bump-version
 
 ## Example Usage:
-
-The following example will do the following:
-1. Only runs on a pull request into main and when any files in the directory `python_project/` have changed or the `pyproject.toml` file has changed.
-2. The job only runs if the pull request is not being merged. This is useful for when a repository is set up to not allow writing directly to main.
-3. It will do a minor version bump to the `pyproject.toml` and commit the changes to the compare branch.
 
 ```yaml
 name: "Version bumper"
@@ -39,19 +27,9 @@ jobs:
           fetch-depth: "0"
 
       - name: Version bumper
-        uses: apowis/pyproject-bump-version@v0.0.1
+        uses: monperrs/pyproject-bump-pypi@v0.0.1
         with:
           file_to_bump: "./pyproject.toml"
           bump_type: "minor"
-          main_branch: "main"
-          python_version: "3.10"
 ```
 
-
-# TODO List
-- Work out more about the fetch depth and potentially using different branches
-- Document `version_compare.py` more and add some tests.
-- Add more options to EndBug/add-and-commit@v9 inputs
-- Add option to not merge or not depending on pull request merge status
-- Implement more file types than pyproject.toml?
-- Long term, think about using npm stuff instead of Python scripts. Probably will never get to doing this.
--- version_compare.py
+++ version_compare.py
@@ -5,26 +5,36 @@ from typing import Literal, Optional, Tuple
 
 from packaging.version import parse, Version
 import toml
+import requests
+import os
 
 
-def get_main_version(project_file: Path, main_branch: Optional[str] = None) -> Version:
+def get_main_version(project_file: Path) -> Version:
     """
-    Uses git cat-file to toml load the pyproject file and returns the verison number
+    Gets the last version on PyPi eg https://pypi.org/pypi/crawler-user-agents/json
+    curl https://pypi.org/pypi/crawler-user-agents/json| jq .info.version
     """
-    main_branch = main_branch if main_branch else "main"
-    pyproject = check_output(["git", "cat-file", "blob", f"origin/{main_branch}:./{project_file}"])
-    version_string = toml.loads(pyproject.decode())["project"]["version"]
-    return parse(version_string)
+    project_name = get_project_name(project_file)
+    pypi_version = requests.get("https://pypi.org/pypi/"+project_name+"/json").json()["info"]["version"]
+    return parse(pypi_version)
 
 
 def get_current_version(pyproject: str) -> Version:
     """
-    Toml loads the pyproject file and returns the verison number.
+    Toml loads the pyproject file and returns the version number.
     """
     with open(pyproject, "r") as fh:
         pyproject_dict = toml.load(fh)
         version_string = pyproject_dict["project"]["version"]
         return parse(version_string)
+
+def get_project_name(pyproject: str) -> Version:
+    """
+    Toml loads the pyproject file and returns the version number.
+    """
+    with open(pyproject, "r") as fh:
+        pyproject_dict = toml.load(fh)
+        return pyproject_dict["project"]["name"]
 
 
 def get_next_version(
@@ -51,7 +61,7 @@ def bump_pyproject(pyproject: Path, new_version_number: str) -> None:
 def parse_args() -> Tuple[str, str, str, Path]:
     """get argparse values"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--main", help="Main branch name", required=True)
+    parser.add_argument("--main", help="Main branch name", required=False)
     parser.add_argument(
         "--bump_type",
         help="Bump type",
@@ -62,7 +72,7 @@ def parse_args() -> Tuple[str, str, str, Path]:
     parser.add_argument(
         "--bump_commit_file",
         help="Path to file containing git commit message",
-        required=True,
+        required=False,
     )
     args = parser.parse_args()
     pyproject = Path(args.pyproject)
@@ -80,22 +90,21 @@ def parse_args() -> Tuple[str, str, str, Path]:
 def main():
     main_branch, bump_type, bump_commit_file, pyproject = parse_args()
 
-    main_version = get_main_version(pyproject, main_branch)
-    current_version = get_current_version(pyproject)
+    main_version = get_main_version(pyproject)
+    local_version = get_current_version(pyproject)
 
-    print(f"{main_branch} branch version: {main_version}")
-    print(f"Current branch version: {current_version}")
-    if current_version <= main_version:
-        new_version = get_next_version(main_version, bump_type)
-        bump_pyproject(pyproject, new_version)
+    new_version = get_next_version(main_version, bump_type)
+    print(f"Pypi local version: {local_version}")
+    print(f"Pypi current version: {main_version}")
+    print(f"Next version: {new_version}")
+    bump_pyproject(pyproject, new_version)
+    if bump_commit_file and os.path.exists(bump_commit_file):
         bump_message = (
-            f"Bumped version from {current_version} to {new_version}"
+            f"Bumped version from {local_version} to {new_version}"
         )
         print(bump_message)
         with open(bump_commit_file, "w") as fh:
             fh.write(bump_message)
-    else:
-        print('No need to bump the version this time.')
 
 
 if __name__ == "__main__":
